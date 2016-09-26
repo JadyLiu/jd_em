44518b5ae5094fa6afcefc48a04cb0e5dad60e57
Liu, Jady <jady.liu@news.com.au>
2016-09-26 17:42:30 +1000

    Add more sets

--- program.py
+++ program.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python
+
 import os
 import csv
 import io
+try:
+    import statistics
+except:
+    import statistics_for_py2 as statistics
+
+from data_types import Purchase
 
 def main():
     print_header()
     filename = get_data_file()
     data = load_file(filename)
     query_data(data)
-    print(data)
 
 def print_header():
     print('---------------------------') 
@@ -21,20 +28,46 @@ def get_data_file():
 
 def load_file(filename):
     with io.open(filename, 'r', encoding='utf-8') as fin:
-        header = fin.readline().strip()
-        print('header found'+ header)
 
+        reader = csv.DictReader(fin)
+        purchases = []
+        for row in reader:
+            p = Purchase.create_from_dict(row)
+            purchases.append(p)
 
-        lines = []
-        for line in fin:
-            line_data = line.strip().split('.')
-            bed_count = line_data[4]
-            lines.append(line_data)
-        print(lines[:5])
+        return purchases
+
+def announce(item,msg):
+    print("Pulling item {} for {}".format(item, msg))
+    return item
 
 def query_data(data):
-    pass
+    data.sort(key=lambda p: p.price)
 
+    high_purchase = data[-1]
+    print("The most expensive house is ${:,} with {} beds and {} baths".format(
+        high_purchase.price, high_purchase.beds, high_purchase.baths))
+    low_purchase = data[0]
+    print("The lease expensive house is ${:,} with {} beds and {} baths".format(
+        low_purchase.price, low_purchase.beds, low_purchase.baths))
+    
+    two_bedroom_homes = (
+        p
+        for p in data
+        if announce(p, "2-bedrooms, found {}".format(p.beds)) and p.beds == 2         
+    )
+
+    homes = []
+    for h in two_bedroom_homes:
+        if len(homes) > 5:
+            break
+        homes.append(h)
+
+    ave_price = statistics.mean((announce(p.price, "price") for p in homes))
+    ave_baths = statistics.mean((p.baths for p in homes))
+    ave_sqft = statistics.mean((p.sq__ft for p in homes))
+    print("The average 2-bedrooms home price is ${:,},bath={},sq ft={:,}".format(int(ave_price),round(ave_baths,1),round(ave_sqft,1)))
 
 if __name__ == '__main__':
     main()
+
